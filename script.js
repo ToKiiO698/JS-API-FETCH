1a77aa70b2f4208f5461eae74a47740fbd0af309
ToKiiO <bilal.dira69@gmail.com>
2024-02-21 12:51:56 +0100

    21/02/2024

---
+++ script.js
@@ -0,0 +1,57 @@
+let tab = [
+  "pommes",
+  "peches",
+  "poire",
+  "banane",
+  "orange",
+  "kiwi",
+  "raisin",
+  "ananas",
+  "cerise",
+];
+let tab2 = [];
+
+document.getElementById(0).addEventListener("click", function () {
+  tab2.push(tab[0]);
+  console.log(tab2);
+});
+
+document.getElementById(1).addEventListener("click", function () {
+  tab2.push(tab[1]);
+  console.log(tab2);
+});
+
+document.getElementById(2).addEventListener("click", function () {
+  tab2.push(tab[2]);
+  console.log(tab2);
+});
+
+document.getElementById(3).addEventListener("click", function () {
+  tab2.push(tab[3]);
+  console.log(tab2);
+});
+
+document.getElementById(4).addEventListener("click", function () {
+  tab2.push(tab[4]);
+  console.log(tab2);
+});
+
+document.getElementById(5).addEventListener("click", function () {
+  tab2.push(tab[5]);
+  console.log(tab2);
+});
+
+document.getElementById(6).addEventListener("click", function () {
+  tab2.push(tab[6]);
+  console.log(tab2);
+});
+
+document.getElementById(7).addEventListener("click", function () {
+  tab2.push(tab[7]);
+  console.log(tab2);
+});
+
+document.getElementById(8).addEventListener("click", function () {
+  tab2.push(tab[8]);
+  console.log(tab2);
+});
